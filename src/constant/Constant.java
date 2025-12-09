10a5e5e866dda22df76d67429688799926252b44
Thái Quang Huy <huyt09122009@gmail.com>
2025-12-09 15:02:41 +0700

    Delete src directory

--- src/constant/Constant.java
+++
@@ -1,7 +0,0 @@
-package constant;
-
-public class Constant {
-    public static class Error {
-        public static final String INVALID_CHOICE = "Invalid choice!";
-    }
-}
--- src/model/Borrowable.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface Borrowable {
-    public boolean borrow(int quantity);
-    public int getAvailableQuantity();
-}
